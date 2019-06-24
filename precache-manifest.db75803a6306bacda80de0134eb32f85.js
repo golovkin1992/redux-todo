e4621404b4c9d4feb95f2760491e20058d06a0f3
User <golova92-69@yandex.ru>
2019-06-25 01:26:47 +0300

    Updates

---
+++ precache-manifest.db75803a6306bacda80de0134eb32f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb01b2e9ea858a07dfc287b0e1d46f81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cca7e3c77dd57806e281",
+    "url": "/static/css/main.95cabfad.chunk.css"
+  },
+  {
+    "revision": "a32e6fc8b418f3e355cc",
+    "url": "/static/js/2.9639922a.chunk.js"
+  },
+  {
+    "revision": "cca7e3c77dd57806e281",
+    "url": "/static/js/main.2097a09b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ef2b84790dbffc19d0e8bbfafbc861e",
+    "url": "/static/media/cheked.6ef2b847.svg"
+  },
+  {
+    "revision": "40e0f797929ce7b185ed654ae67b7f13",
+    "url": "/static/media/not-checked.40e0f797.svg"
+  }
+]);
